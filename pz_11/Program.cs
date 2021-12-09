c18206a3921157a01e4cac226d5885b5aa42dd79
19032004katya@gmail.com <19032004katya@gmail.com>
2021-12-09 16:15:32 +0300

    pz11

---
+++ pz_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "быть или не быть вот в чем вопрос";
+            Console.WriteLine(ReverseWords(str));
+            Console.ReadKey();
+        }
+        static string ReverseWords(string str)
+        {
+            string[] words = str.Split();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = new string(words[i].Reverse().ToArray());
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
