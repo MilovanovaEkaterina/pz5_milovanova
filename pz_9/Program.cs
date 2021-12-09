a03fb56f4b5e81b6e3053b461952f65e87d46ace
19032004katya@gmail.com <19032004katya@gmail.com>
2021-12-09 11:20:19 +0300

    pz_9

---
+++ pz_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] arr = new double[6, 7];
+            var s = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    arr[i, j] = (double)s.Next(100) / 10;
+                }
+            }
+        }
+
+    }
+}
