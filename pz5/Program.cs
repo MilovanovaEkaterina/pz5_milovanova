97d78e184b3753c2bf1226a7333b30720991d1ab
19032004katya@gmail.com <19032004katya@gmail.com>
2021-12-09 10:58:59 +0300

    test edit

--- pz5/Program.cs
+++ pz5/Program.cs
@@ -10,72 +10,75 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Задание 1:\n");
-            int r, n, m, t;
-            for (r = 10; r <= 100; r = r + 5)
+            Console.WriteLine("работа с git");
+
             {
-                Console.WriteLine(r + " ");
-                Console.WriteLine("\nЗадание №2:\n");
-                n = 0;
-                char smv = 'А';
-                while (n <= 5)
+                Console.WriteLine("Задание 1:\n");
+                int r, n, m, t;
+                for (r = 10; r <= 100; r = r + 5)
                 {
-                    n += 1;
-                    Console.Write(smv + " ");
-                    smv++;
-                    Console.WriteLine("\n\nЗадание 3:\n");
-                    n = 6;
-                    m = 7;
-                    for (n = 0; n <= 6; n++)
+                    Console.WriteLine(r + " ");
+                    Console.WriteLine("\nЗадание №2:\n");
+                    n = 0;
+                    char smv = 'А';
+                    while (n <= 5)
                     {
-                        for (m = 0; m <= 7; m++)
+                        n += 1;
+                        Console.Write(smv + " ");
+                        smv++;
+                        Console.WriteLine("\n\nЗадание 3:\n");
+                        n = 6;
+                        m = 7;
+                        for (n = 0; n <= 6; n++)
                         {
-                            Console.Write("#");
+                            for (m = 0; m <= 7; m++)
+                            {
+                                Console.Write("#");
+                            }
+                            Console.WriteLine();
                         }
-                        Console.WriteLine();
-                    }
-                    Console.WriteLine("\nЗадание 4:\n");
-                    for (r = 0; r <= 100; r++)
-                    {
-                        if (r == 0) continue;
-                        if ((r % 6) != 0) continue;
-                        Console.WriteLine(r + " ");
+                        Console.WriteLine("\nЗадание 4:\n");
+                        for (r = 0; r <= 100; r++)
+                        {
+                            if (r == 0) continue;
+                            if ((r % 6) != 0) continue;
+                            Console.WriteLine(r + " ");
+
+                        }
+                        Console.WriteLine("\nЗадание 5:\n");
+                        r = 3;
+                        t = 50;
+                        for (r = 3, t = 50; t - r > 27; r++, t--)
+                        {
+                            Console.WriteLine("r и t равны:" + r + "и" + t);
+                        }
+
 
                     }
-                    Console.WriteLine("\nЗадание 5:\n");
-                    r = 3;
-                    t = 50;
-                    for (r = 3, t = 50; t - r > 27; r++, t--)
-                    {
-                        Console.WriteLine("r и t равны:" + r + "и" + t);
-                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             }
+
+
+
+
+
         }
 
 
-
-
-
     }
-
-
-
 }
