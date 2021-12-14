3923e9632f746609e37d34fa695526aa64810836
MilovanovaEkaterina <95638401+MilovanovaEkaterina@users.noreply.github.com>
2021-12-14 15:31:29 +0300

    Update Program.cs

--- pz_6/Program.cs
+++ pz_6/Program.cs
@@ -9,7 +9,16 @@ namespace pz_6
     class Program
     {
         static void Main(string[] args)
-        {
+        {Console.WriteLine("Введите число");
+int k=Convert.ToInt32(Console.ReadLine());
+double l = 1.0;
+for(int y=2;y<k+1;y++)
+{
+l += (1 /Convert.ToDouble(y));
+}
+Console.WriteLine(l);
+
+
         }
     }
 }
